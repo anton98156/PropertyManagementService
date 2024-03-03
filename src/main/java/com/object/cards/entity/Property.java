46f1f2d29a453b1f85fc281bfe241e1c2439d4d9
Anton Kravtsov <113596566+anton98156@users.noreply.github.com>
2024-03-03 12:37:23 +0300

    Update Property.java
    Added Property main fields

--- src/main/java/com/object/cards/entity/Property.java
+++ src/main/java/com/object/cards/entity/Property.java
@@ -1,5 +1,33 @@
 package com.object.cards.entity;
 
 public class Property {
-    
+
+    private int id;
+    private String address;
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 }
